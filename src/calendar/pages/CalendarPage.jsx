8cfcd6137df427a98b3e57f82baf063fa4cc8b10
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-12-27 18:16:19 -0600

    chore: start for calendarpage

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,46 @@
-import { NavBar } from "../components"
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours, parse, startOfWeek, getDay, format } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+import { NavBar } from "../components";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "My event",
+    notes: "Some notes",
+    start: new Date(),
+    end: addHours(new Date(), 1),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Raul Angel",
+    },
+  },
+];
 
 export const CalendarPage = () => {
   return (
     <>
       <NavBar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+      />
     </>
-  )
-}
+  );
+};
