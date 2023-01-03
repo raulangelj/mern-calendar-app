08269ef4ff9a49a56ccce89248e038b9ef96ddae
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-01-02 18:23:31 -0600

    ADD auth hook with login

---
+++ src/shared/hooks/useAuthStore/useAuthStore.js
@@ -0,0 +1,38 @@
+import { useDispatch, useSelector } from "react-redux";
+import calendarAPI from "../../../api/calendarAPI";
+import {
+  clearErrorMessage,
+  onChecking,
+  onLogin,
+  onLogout,
+} from "../../../store";
+
+export const useAuthStore = () => {
+  const { status, user, errorMessage } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const startLogin = async ({ email, password }) => {
+    console.log("startLogin", email, password);
+    dispatch(onChecking());
+    try {
+      const { data } = await calendarAPI.post("/auth", { email, password });
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid, email }));
+    } catch (error) {
+      // console.log(error);
+      dispatch(onLogout("Email or password incorrect"));
+      // WE USE THE TIMEOUT TO CLEAR THE ERROR AFTER SOME TIME AND ALLOW THE USER TO TRY AGAIN
+      setTimeout(() => {
+        dispatch(clearErrorMessage());
+      }, 10);
+    }
+  };
+
+  return {
+    status,
+    user,
+    errorMessage,
+    startLogin,
+  };
+};
