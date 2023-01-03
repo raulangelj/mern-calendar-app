f98e2b650b6bd6e49e28d01b431296f22b5b65fa
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-01-02 19:18:05 -0600

    FEAT login functional

--- src/auth/pages/LoginPage/LoginPage.jsx
+++ src/auth/pages/LoginPage/LoginPage.jsx
@@ -1,17 +1,73 @@
+import { useEffect } from "react";
+import Swal from "sweetalert2";
+import { useAuthStore, useForm } from "../../../shared/hooks";
 import "./LoginPage.css";
 
+const loginFormFields = {
+  loginEmail: "",
+  loginPassword: "",
+};
+
+const signInFormFields = {
+  signinName: "",
+  signinEmail: "",
+  signinPassword: "",
+  signinPassword2: "",
+};
+
 export const LoginPage = () => {
+  const { startLogin, errorMessage, startSignin } = useAuthStore();
+  const {
+    loginEmail,
+    loginPassword,
+    onInputChange: onLoginInputChange,
+  } = useForm(loginFormFields);
+  const {
+    signinName,
+    signinEmail,
+    signinPassword,
+    signinPassword2,
+    onInputChange: onSigninInputChange,
+  } = useForm(signInFormFields);
+
+  useEffect(() => {
+    if (errorMessage !== undefined) {
+      Swal.fire("Error", errorMessage, "error");
+    }
+  }, [errorMessage]);
+
+  const loginSubmit = (e) => {
+    e.preventDefault();
+    startLogin({ email: loginEmail, password: loginPassword });
+  };
+
+  const signinSubmite = (e) => {
+    e.preventDefault();
+    if (signinPassword !== signinPassword2) {
+      Swal.fire("Error on Signin", "Passwords are not the same", "error");
+      return;
+    }
+    startSignin({
+      name: signinName,
+      email: signinEmail,
+      password: signinPassword,
+    })
+  };
+
   return (
     <div className="container login-container">
       <div className="row">
         <div className="col-md-6 login-form-1">
           <h3>Ingreso</h3>
-          <form>
+          <form onSubmit={loginSubmit}>
             <div className="form-group mb-2">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Correo"
+                name="loginEmail"
+                value={loginEmail}
+                onChange={onLoginInputChange}
               />
             </div>
             <div className="form-group mb-2">
@@ -19,6 +75,9 @@ export const LoginPage = () => {
                 type="password"
                 className="form-control"
                 placeholder="Contraseña"
+                name="loginPassword"
+                value={loginPassword}
+                onChange={onLoginInputChange}
               />
             </div>
             <div className="d-grid gap-2">
@@ -29,12 +88,15 @@ export const LoginPage = () => {
 
         <div className="col-md-6 login-form-2">
           <h3>Registro</h3>
-          <form>
+          <form onSubmit={signinSubmite}>
             <div className="form-group mb-2">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Nombre"
+                name="signinName"
+                value={signinName}
+                onChange={onSigninInputChange}
               />
             </div>
             <div className="form-group mb-2">
@@ -42,6 +104,9 @@ export const LoginPage = () => {
                 type="email"
                 className="form-control"
                 placeholder="Correo"
+                name="signinEmail"
+                value={signinEmail}
+                onChange={onSigninInputChange}
               />
             </div>
             <div className="form-group mb-2">
@@ -49,6 +114,9 @@ export const LoginPage = () => {
                 type="password"
                 className="form-control"
                 placeholder="Contraseña"
+                name="signinPassword"
+                value={signinPassword}
+                onChange={onSigninInputChange}
               />
             </div>
 
@@ -57,6 +125,9 @@ export const LoginPage = () => {
                 type="password"
                 className="form-control"
                 placeholder="Repita la contraseña"
+                name="signinPassword2"
+                value={signinPassword2}
+                onChange={onSigninInputChange}
               />
             </div>
 
