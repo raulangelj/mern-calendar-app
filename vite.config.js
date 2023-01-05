5a1a27c7ae2202be0f3a004c05d6938feff74d33
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-01-05 12:45:19 -0600

    UPDATE for single page app

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  appType: "spa",
   plugins: [react({include: '**/*.jsx'}), EnvironmentPlugin("all")],
   server: {
     port: 3000,
